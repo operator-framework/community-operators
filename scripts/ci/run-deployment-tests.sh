ea3ace5188b002ac76ba2f6107dbb5ccd7b0996b
j0zi <jbreza@redhat.com>
2020-04-20 10:05:42 +0200

    forcing build in case of no diff, which means merged
    Signed-off-by: j0zi <jbreza@redhat.com>

--- scripts/ci/run-deployment-tests.sh
+++ scripts/ci/run-deployment-tests.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
 eval $(scripts/ci/operators-env)
-      
-if [ -z "${OP_PATH}" ] ; 
-then 
+
+if [ -z "${IS_TESTABLE}" ];
+then
+    if [ -z "${OP_PATH}" ] ;
+  then
     echo "No operator modification detected. Exiting."
-    exit 0 
-else
+    exit 0
+  else
     echo "Detected modified Operator in ${OP_PATH}"
     echo "Detected modified Operator version ${OP_VER}"
+  fi
+
+  make operator.install OP_PATH="${OP_PATH}" OP_VER="${OP_VER}"
+
+else
+  if [[ $IS_TESTABLE -eq 0 ]]; then
+   echo "Nothing was changed, not running deployment tests."
+    exit 0
+  fi
 fi
-      
-make operator.install OP_PATH="${OP_PATH}" OP_VER="${OP_VER}"
