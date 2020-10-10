df8cd3c24d0d2ddda9cef5677824f7bc6cbd6766
Martin Vala <martin.vala@hotovo.com>
2020-10-10 07:07:03 +0200

    Impoved script for operatorhub.io
    Signed-off-by: Martin Vala <martin.vala@hotovo.com>

--- scripts/ci/trigger-operatorhubio-ci.sh
+++ scripts/ci/trigger-operatorhubio-ci.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-
+CI_OHIO_BRANCH=${CI_OHIO_BRANCH-"master"}
 curl -s -X POST \
      -H "Content-Type: application/json" \
      -H "Accept: application/json" \
      -H "Travis-API-Version: 3" \
      -H "Authorization: token $CI_OHIO_TRIGGER_TOKEN"  \
-     -d '{"request":{"branch":"master"}}'  \
+     -d "{\"request\":{\"branch\":\"$CI_OHIO_BRANCH\"}}"  \
      https://api.travis-ci.com/repo/operator-framework%2Foperatorhub.io/requests
 echo -e "\nCI on operator-framework/operatorhub.io and it's master branch was triggered"
