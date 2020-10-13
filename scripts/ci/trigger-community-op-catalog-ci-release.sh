7ac69d319f5de30a7f05a6d59a824a87dc424276
Martin Vala <martin.vala@hotovo.com>
2020-10-13 08:58:12 +0200

    Added better messages for http requests
    Signed-off-by: Martin Vala <martin.vala@hotovo.com>

--- scripts/ci/trigger-community-op-catalog-ci-release.sh
+++ scripts/ci/trigger-community-op-catalog-ci-release.sh
@@ -1,11 +1,12 @@
-
 #!/usr/bin/env bash
+
+eval $(scripts/ci/ansible-env)
 
 curl -s -X POST \
      -H "Content-Type: application/json" \
      -H "Accept: application/json" \
      -H "Travis-API-Version: 3" \
      -H "Authorization: token $FRAMEWORK_AUTOMATION_ON_TRAVIS"  \
-     -d '{"request":{"branch":"master"}}'  \
+     -d "{\"request\":{\"branch\":\"master\",\"message\":\"$OP_PATH ($OP_VER)\"}}"  \
      https://api.travis-ci.com/repo/operator-framework%2Fcommunity-operator-catalog/requests
 echo -e "\nRelease pipeline has been triggered on operator-framework/community-operator-catalog"
