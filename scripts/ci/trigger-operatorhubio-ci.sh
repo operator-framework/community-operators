4f85b76ef930f1172b210990dfa1665e055dfda0
mvalahtv <64977763+mvalahtv@users.noreply.github.com>
2020-10-13 09:08:00 +0200

    Added better messages for http requests (#2457)
    Signed-off-by: Martin Vala <martin.vala@hotovo.com>

--- scripts/ci/trigger-operatorhubio-ci.sh
+++ scripts/ci/trigger-operatorhubio-ci.sh
@@ -5,6 +5,6 @@ curl -s -X POST \
      -H "Accept: application/json" \
      -H "Travis-API-Version: 3" \
      -H "Authorization: token $CI_OHIO_TRIGGER_TOKEN"  \
-     -d "{\"request\":{\"branch\":\"$CI_OHIO_BRANCH\"}}"  \
+     -d "{\"request\":{\"branch\":\"$CI_OHIO_BRANCH\",\"message\":\"Triggering operatorhub.io on $CI_OHIO_BRANCH branch\"}}"  \
      https://api.travis-ci.com/repo/operator-framework%2Foperatorhub.io/requests
 echo -e "\nCI on operator-framework/operatorhub.io and it's '$CI_OHIO_BRANCH' branch was triggered"
