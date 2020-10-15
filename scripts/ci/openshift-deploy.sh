ac09a8d9b60879c8c559622f379d6f634050765c
J0zi <jbreza@redhat.com>
2020-10-15 10:43:44 +0200

    Enhance openshift exit (#2487)
    Signed-off-by: j0zi <jbreza@redhat.com>

--- scripts/ci/openshift-deploy.sh
+++ scripts/ci/openshift-deploy.sh
@@ -3,6 +3,7 @@
 
 set -e #fail in case of non zero return
 
+DO_NOT_RUN=false
 export PATH=$PATH:/tmp/operator-test/bin
 mkdir -p /tmp/operator-test/bin
 curl https://mirror.openshift.com/pub/openshift-v4/clients/oc/4.6/linux/oc.tar.gz | tar xvzf - -C /tmp/operator-test/bin oc
@@ -48,26 +49,30 @@ git --no-pager log -m -1 --name-only --first-parent $COMMIT
 declare -A CHANGED_FILES
 ##community only
 echo "changed community files:"
-CHANGED_FILES=$(git --no-pager log -m -1 --name-only --first-parent $COMMIT|grep -v 'upstream-community-operators/'|grep 'community-operators/') || ( echo '******* No community operator (Openshift) modified, no reason to deploy on Openshift *******'; exit 0)
+CHANGED_FILES=$(git --no-pager log -m -1 --name-only --first-parent $COMMIT|grep -v 'upstream-community-operators/'|grep 'community-operators/') || ( echo '******* No community operator (Openshift) modified, no reason to deploy on Openshift *******'; DO_NOT_RUN=true; exit 0)
 echo
 
-for sf in ${CHANGED_FILES[@]}; do
-  echo $sf
-  if [ $(echo $sf| awk -F'/' '{print NF}') -ge 4 ]; then
-      OP_NAME="$(echo "$sf" | awk -F'/' '{ print $2 }')"
-      OP_VER="$(echo "$sf" | awk -F'/' '{ print $3 }')"
-  fi
-done
-echo
-echo "OP_NAME=$OP_NAME"
-echo "OP_VER=$OP_VER"
+echo "DO_NOT_RUN=$DO_NOT_RUN"
+if [ "$DO_NOT_RUN" = false ] ; then
 
-#detection end
+  for sf in ${CHANGED_FILES[@]}; do
+    echo $sf
+    if [ $(echo $sf| awk -F'/' '{print NF}') -ge 4 ]; then
+        OP_NAME="$(echo "$sf" | awk -F'/' '{ print $2 }')"
+        OP_VER="$(echo "$sf" | awk -F'/' '{ print $3 }')"
+    fi
+  done
+  echo
+  echo "OP_NAME=$OP_NAME"
+  echo "OP_VER=$OP_VER"
 
-mkdir -p /tmp/playbooks2
-cd /tmp/playbooks2
-ansible-pull -d /tmp/.ansible-pulled -vv -U https://github.com/J0zi/operator-test-playbooks -C RHO-716-deploy-on-openshift -vv -i localhost, deploy-olm-operator-openshift-upstream.yml -e ansible_connection=local -e package_name=$OP_NAME -e operator_dir=$TARGET_PATH/$OP_NAME -e op_version=$OP_VER
-echo "Variable summary:"
-echo "OP_NAME=$OP_NAME"
-echo "OP_VER=$OP_VER"
-#echo "CSV_FILE=$CSV_FILE"
+  #detection end
+
+  mkdir -p /tmp/playbooks2
+  cd /tmp/playbooks2
+  ansible-pull -d /tmp/.ansible-pulled -vv -U https://github.com/J0zi/operator-test-playbooks -C RHO-716-deploy-on-openshift -vv -i localhost, deploy-olm-operator-openshift-upstream.yml -e ansible_connection=local -e package_name=$OP_NAME -e operator_dir=$TARGET_PATH/$OP_NAME -e op_version=$OP_VER
+  echo "Variable summary:"
+  echo "OP_NAME=$OP_NAME"
+  echo "OP_VER=$OP_VER"
+
+fi
