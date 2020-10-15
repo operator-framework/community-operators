6d5a762458656797db641e705ea0194633fb8eb0
Martin Vala <martin.vala@hotovo.com>
2020-10-15 09:13:59 +0200

    Triggering different branches in catalog project
    Signed-off-by: Martin Vala <martin.vala@hotovo.com>

--- scripts/ci/trigger-community-op-catalog-ci-release.sh
+++ scripts/ci/trigger-community-op-catalog-ci-release.sh
@@ -7,6 +7,6 @@ curl -s -X POST \
      -H "Accept: application/json" \
      -H "Travis-API-Version: 3" \
      -H "Authorization: token $FRAMEWORK_AUTOMATION_ON_TRAVIS"  \
-     -d "{\"request\":{\"branch\":\"master\",\"message\":\"$OP_PATH ($OP_VER)\"}}"  \
+     -d "{\"request\":{\"branch\":\"$STREAM_NAME\",\"message\":\"$OP_PATH ($OP_VER)\"}}"  \
      https://api.travis-ci.com/repo/operator-framework%2Fcommunity-operator-catalog/requests
 echo -e "\nRelease pipeline has been triggered on operator-framework/community-operator-catalog"
