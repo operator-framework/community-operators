382543aee1d94d648a0b072af99e288919e11c38
J0zi <jbreza@redhat.com>
2020-10-21 18:09:04 +0200

    force aqua (#2527)
    Signed-off-by: j0zi <jbreza@redhat.com>

--- scripts/ci/openshift-deploy.sh
+++ scripts/ci/openshift-deploy.sh
@@ -63,27 +63,30 @@ echo "changed community files:"
 CHANGED_FILES=$(git --no-pager log -m -1 --name-only --first-parent $COMMIT|grep -v 'upstream-community-operators/'|grep 'community-operators/') || { echo '******* No community operator (Openshift) modified, no reason to deploy on Openshift *******'; DO_NOT_RUN=true; exit 0; }
 echo
 
-echo "DO_NOT_RUN=$DO_NOT_RUN"
-if [ "$DO_NOT_RUN" = false ] ; then
+for sf in ${CHANGED_FILES[@]}; do
+  echo $sf
+  if [ $(echo $sf| awk -F'/' '{print NF}') -ge 4 ]; then
+      OP_NAME="$(echo "$sf" | awk -F'/' '{ print $2 }')"
+      OP_VER="$(echo "$sf" | awk -F'/' '{ print $3 }')"
+  fi
+done
+echo
+echo "OP_NAME=$OP_NAME"
+echo "OP_VER=$OP_VER"
 
-  for sf in ${CHANGED_FILES[@]}; do
-    echo $sf
-    if [ $(echo $sf| awk -F'/' '{print NF}') -ge 4 ]; then
-        OP_NAME="$(echo "$sf" | awk -F'/' '{ print $2 }')"
-        OP_VER="$(echo "$sf" | awk -F'/' '{ print $3 }')"
-    fi
-  done
-  echo
-  echo "OP_NAME=$OP_NAME"
-  echo "OP_VER=$OP_VER"
+#detection end
 
-  #detection end
+#TODO: comment out
+#test
+OP_NAME=aqua
+OP_VER=1.0.2
+COMMIT=1234
+echo "Forced specific operator - $OP_NAME $OP_VER $COMMIT"
 
-  mkdir -p /tmp/playbooks2
-  cd /tmp/playbooks2
-  ansible-pull -d /tmp/.ansible-pulled -vv -U https://github.com/J0zi/operator-test-playbooks -C RHO-716-deploy-on-openshift -vv -i localhost, deploy-olm-operator-openshift-upstream.yml -e ansible_connection=local -e package_name=$OP_NAME -e operator_dir=$TARGET_PATH/$OP_NAME -e op_version=$OP_VER -e oc_bin_path=oc $SUBDIR_ARG
-  echo "Variable summary:"
-  echo "OP_NAME=$OP_NAME"
-  echo "OP_VER=$OP_VER"
-
-fi
+#deploy start
+mkdir -p /tmp/playbooks2
+cd /tmp/playbooks2
+ansible-pull -d /tmp/.ansible-pulled -vv -U https://github.com/J0zi/operator-test-playbooks -C RHO-716-deploy-on-openshift -vv -i localhost, deploy-olm-operator-openshift-upstream.yml -e ansible_connection=local -e package_name=$OP_NAME -e operator_dir=$TARGET_PATH/$OP_NAME -e op_version=$OP_VER -e oc_bin_path=oc -e commit_tag=$COMMIT $SUBDIR_ARG
+echo "Variable summary:"
+echo "OP_NAME=$OP_NAME"
+echo "OP_VER=$OP_VER"
