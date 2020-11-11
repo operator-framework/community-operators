1d66ce099ee910766be537cc7e09385cd94f838e
mvalarh <73172904+mvalarh@users.noreply.github.com>
2020-11-11 14:28:45 +0100

    Http request to overwrite operator version (#2648)
    * Http request to overwrite operator version
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Improved branch name
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Added delete operator http request
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Testing version for PR parsing
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Handling pull requests
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Rename to OP_VER_OVERWRITE
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * env to source file
    
    Signed-off-by: Martin Vala <mavala@redhat.com>

--- scripts/ci/trigger-community-op-catalog-ci-release.sh
+++ scripts/ci/trigger-community-op-catalog-ci-release.sh
@@ -10,6 +10,9 @@ if [ -f /tmp/vars-op-path ]; then
        exit 0 # no need to test/release, no operator modified
      fi
 
+     echo "STREAM_NAME=$STREAM_NAME OP_NAME=$OP_NAME OP_VER=$OP_VER"
+     echo "PR_NUMBER=$PR_NUMBER OP_VER_OVERWRITE=$OP_VER_OVERWRITE OP_RECREATE=$OP_RECREATE"
+
      curl -s -X POST \
      -H "Content-Type: application/json" \
      -H "Accept: application/json" \
@@ -24,3 +27,24 @@ else
      exit 1
 fi
 
+
+
+# This will execute version overwrite
+# curl -s -X POST \
+#      -H "Content-Type: application/json" \
+#      -H "Accept: application/json" \
+#      -H "Travis-API-Version: 3" \
+#      -H "Authorization: token $FRAMEWORK_AUTOMATION_ON_TRAVIS"  \
+#      -d "{\"request\":{\"branch\":\"job/$STREAM_NAME-update\",\"message\":\"$OP_NAME ($OP_VER)\",\"config\":{\"env\":{\"jobs\":[\"OP_STREAM_NAME_VER=$STREAM_NAME/$OP_NAME/$OP_VER\"]}}}}"  \
+#      https://api.travis-ci.com/repo/operator-framework%2Fcommunity-operator-catalog/requests
+#      echo -e "\nRelease pipeline has been triggered on operator-framework/community-operator-catalog"
+
+# This will execute operator delet
+# curl -s -X POST \
+#      -H "Content-Type: application/json" \
+#      -H "Accept: application/json" \
+#      -H "Travis-API-Version: 3" \
+#      -H "Authorization: token $FRAMEWORK_AUTOMATION_ON_TRAVIS"  \
+#      -d "{\"request\":{\"branch\":\"job/$STREAM_NAME-update\",\"message\":\"$OP_NAME ($OP_VER)\",\"config\":{\"env\":{\"jobs\":[\"OP_STREAM_NAME_VER=$STREAM_NAME/$OP_NAME\",\"OP_DELETE=1\"]}}}}"  \
+#      https://api.travis-ci.com/repo/operator-framework%2Fcommunity-operator-catalog/requests
+#      echo -e "\nRelease pipeline has been triggered on operator-framework/community-operator-catalog"
