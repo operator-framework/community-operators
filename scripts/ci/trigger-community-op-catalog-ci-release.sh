e8c276746da14db5739c341b14ef9310273ad60c
Martin Vala <mavala@redhat.com>
2020-11-11 07:05:27 +0100

    Http request to overwrite operator version
    Signed-off-by: Martin Vala <mavala@redhat.com>

--- scripts/ci/trigger-community-op-catalog-ci-release.sh
+++ scripts/ci/trigger-community-op-catalog-ci-release.sh
@@ -24,3 +24,13 @@ else
      exit 1
 fi
 
+
+# This will execute version overwrite
+# curl -s -X POST \
+#      -H "Content-Type: application/json" \
+#      -H "Accept: application/json" \
+#      -H "Travis-API-Version: 3" \
+#      -H "Authorization: token $FRAMEWORK_AUTOMATION_ON_TRAVIS"  \
+#      -d "{\"request\":{\"branch\":\"$STREAM_NAME\",\"message\":\"$OP_NAME ($OP_VER)\",\"config\":{\"env\":{\"jobs\":[\"OP_STREAM_NAME_VER=$STREAM_NAME/$OP_NAME/$OP_VER\"]}}}}"  \
+#      https://api.travis-ci.com/repo/operator-framework%2Fcommunity-operator-catalog/requests
+#      echo -e "\nRelease pipeline has been triggered on operator-framework/community-operator-catalog"
