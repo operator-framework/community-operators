84401f938cbe0ca9cbb7ad6195bc3af1f2ced928
mvalarh <73172904+mvalarh@users.noreply.github.com>
2020-11-12 10:35:00 +0100

    Operator version overwrite and operator delete (#2657)
    * Operator version overwrite and operator delete
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Fix condition
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Fix recreate
    
    Signed-off-by: Martin Vala <mavala@redhat.com>
    
    * Cosmetics
    
    Signed-off-by: Martin Vala <mavala@redhat.com>

--- scripts/ci/trigger-community-op-catalog-ci-release.sh
+++ scripts/ci/trigger-community-op-catalog-ci-release.sh
@@ -2,49 +2,51 @@
 set -e
 
 NO_OPERATOR=0 #related to ansible-env
-scripts/ci/ansible-env release
+INPUT_ENV_SCRIPT=${INPUT_ENV_SCRIPT-"/tmp/vars-op-path"}
+scripts/ci/ansible-env release || { echo "Error in preparing operator environment !!! Contact admins."; exit 1; }
+source $INPUT_ENV_SCRIPT
 
-if [ -f /tmp/vars-op-path ]; then
-     source /tmp/vars-op-path
-     if [ "$NO_OPERATOR" -gt "0"  ]; then
-       exit 0 # no need to test/release, no operator modified
-     fi
+if [ "$NO_OPERATOR" -gt "0"  ]; then
+  exit 0 # no need to test/release, no operator modified
+fi
 
-     echo "STREAM_NAME=$STREAM_NAME OP_NAME=$OP_NAME OP_VER=$OP_VER"
-     echo "PR_NUMBER=$PR_NUMBER OP_VER_OVERWRITE=$OP_VER_OVERWRITE OP_RECREATE=$OP_RECREATE"
+[[ $OP_VER_OVERWRITE -eq 1 ]] && [[ $OP_RECREATE -eq 1 ]] && { echo "Labels 'allow/operator-version-overwrite' and 'allow/operator-recreate' cannot be set simultaneously !!!"; exit 1; }
 
-     curl -s -X POST \
-     -H "Content-Type: application/json" \
-     -H "Accept: application/json" \
-     -H "Travis-API-Version: 3" \
-     -H "Authorization: token $FRAMEWORK_AUTOMATION_ON_TRAVIS"  \
-     -d "{\"request\":{\"branch\":\"$STREAM_NAME\",\"message\":\"$OP_NAME ($OP_VER)\"}}"  \
-     https://api.travis-ci.com/repo/operator-framework%2Fcommunity-operator-catalog/requests
-     echo -e "\nRelease pipeline has been triggered on operator-framework/community-operator-catalog"
+echo "STREAM_NAME=$STREAM_NAME OP_NAME=$OP_NAME OP_VER=$OP_VER"
+echo "OP_PR_NUMBER=$OP_PR_NUMBER OP_VER_OVERWRITE=$OP_VER_OVERWRITE OP_RECREATE=$OP_RECREATE"
 
-else
-     echo "The file /tmp/vars-op-path does not exist."
-     exit 1
+if [[ $OP_VER_OVERWRITE -eq 1 ]];then
+  # This will execute version overwrite
+  curl -s -X POST \
+      -H "Content-Type: application/json" \
+      -H "Accept: application/json" \
+      -H "Travis-API-Version: 3" \
+      -H "Authorization: token $FRAMEWORK_AUTOMATION_ON_TRAVIS"  \
+      -d "{\"request\":{\"branch\":\"job/$STREAM_NAME-update\",\"message\":\"$OP_NAME ($OP_VER) [OVERWRITE]\",\"config\":{\"env\":{\"jobs\":[\"OP_STREAM_NAME_VER=$STREAM_NAME/$OP_NAME/$OP_VER\"]}}}}"  \
+      https://api.travis-ci.com/repo/operator-framework%2Fcommunity-operator-catalog/requests
+      echo -e "\nRelease pipeline has been triggered on operator-framework/community-operator-catalog"
+  exit 0
 fi
 
 
+if [[ $OP_RECREATE -eq 1 ]];then
+  # This will execute operator delete
+  curl -s -X POST \
+       -H "Content-Type: application/json" \
+       -H "Accept: application/json" \
+       -H "Travis-API-Version: 3" \
+       -H "Authorization: token $FRAMEWORK_AUTOMATION_ON_TRAVIS"  \
+       -d "{\"request\":{\"branch\":\"job/$STREAM_NAME-update\",\"message\":\"$OP_NAME [DELETE]\",\"config\":{\"env\":{\"jobs\":[\"OP_STREAM_NAME_VER=$STREAM_NAME/$OP_NAME\",\"OP_DELETE=1\"]}}}}"  \
+       https://api.travis-ci.com/repo/operator-framework%2Fcommunity-operator-catalog/requests
+       echo -e "\nRelease pipeline has been triggered on operator-framework/community-operator-catalog"
+  sleep 10
+fi
 
-# This will execute version overwrite
-# curl -s -X POST \
-#      -H "Content-Type: application/json" \
-#      -H "Accept: application/json" \
-#      -H "Travis-API-Version: 3" \
-#      -H "Authorization: token $FRAMEWORK_AUTOMATION_ON_TRAVIS"  \
-#      -d "{\"request\":{\"branch\":\"job/$STREAM_NAME-update\",\"message\":\"$OP_NAME ($OP_VER)\",\"config\":{\"env\":{\"jobs\":[\"OP_STREAM_NAME_VER=$STREAM_NAME/$OP_NAME/$OP_VER\"]}}}}"  \
-#      https://api.travis-ci.com/repo/operator-framework%2Fcommunity-operator-catalog/requests
-#      echo -e "\nRelease pipeline has been triggered on operator-framework/community-operator-catalog"
-
-# This will execute operator delet
-# curl -s -X POST \
-#      -H "Content-Type: application/json" \
-#      -H "Accept: application/json" \
-#      -H "Travis-API-Version: 3" \
-#      -H "Authorization: token $FRAMEWORK_AUTOMATION_ON_TRAVIS"  \
-#      -d "{\"request\":{\"branch\":\"job/$STREAM_NAME-update\",\"message\":\"$OP_NAME ($OP_VER)\",\"config\":{\"env\":{\"jobs\":[\"OP_STREAM_NAME_VER=$STREAM_NAME/$OP_NAME\",\"OP_DELETE=1\"]}}}}"  \
-#      https://api.travis-ci.com/repo/operator-framework%2Fcommunity-operator-catalog/requests
-#      echo -e "\nRelease pipeline has been triggered on operator-framework/community-operator-catalog"
+curl -s -X POST \
+-H "Content-Type: application/json" \
+-H "Accept: application/json" \
+-H "Travis-API-Version: 3" \
+-H "Authorization: token $FRAMEWORK_AUTOMATION_ON_TRAVIS"  \
+-d "{\"request\":{\"branch\":\"$STREAM_NAME\",\"message\":\"$OP_NAME ($OP_VER) [RELEASE]\"}}"  \
+https://api.travis-ci.com/repo/operator-framework%2Fcommunity-operator-catalog/requests
+echo -e "\nRelease pipeline has been triggered on operator-framework/community-operator-catalog"
