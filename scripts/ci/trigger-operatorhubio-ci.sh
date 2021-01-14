37e8f8e1c342935c6c59370e7111d1d3fba877ad
J0zi <jbreza@redhat.com>
2021-01-14 14:33:36 +0100

    fix quotes (#2974)
    Signed-off-by: j0zi <jbreza@redhat.com>

--- scripts/ci/trigger-operatorhubio-ci.sh
+++ scripts/ci/trigger-operatorhubio-ci.sh
@@ -4,7 +4,7 @@ curl -s -X POST \
   --url https://api.github.com/repos/operator-framework/operatorhub.io/actions/workflows/4934257/dispatches \
   --header "Authorization: token $REPO_GHA_PAT" \
   --header 'Content-Type: application/json' \
-  --data "{
-    'ref': \"$CI_OHIO_BRANCH\"
-}"
+  --data '{
+    "ref": "$CI_OHIO_BRANCH"
+}'
 echo -e "\nCI on operator-framework/operatorhub.io and it's '$CI_OHIO_BRANCH' branch was triggered"
