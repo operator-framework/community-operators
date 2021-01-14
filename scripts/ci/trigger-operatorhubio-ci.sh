9e2ff1212ce3bf33c54cdf40a28edc62ba2292ff
j0zi <jbreza@redhat.com>
2021-01-14 13:38:44 +0100

    operatorhub.io uses Actions not Travis
    Signed-off-by: j0zi <jbreza@redhat.com>

--- scripts/ci/trigger-operatorhubio-ci.sh
+++ scripts/ci/trigger-operatorhubio-ci.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 CI_OHIO_BRANCH=${CI_OHIO_BRANCH-"master"}
 curl -s -X POST \
-     -H "Content-Type: application/json" \
-     -H "Accept: application/json" \
-     -H "Travis-API-Version: 3" \
-     -H "Authorization: token $CI_OHIO_TRIGGER_TOKEN"  \
-     -d "{\"request\":{\"branch\":\"$CI_OHIO_BRANCH\",\"message\":\"Triggering operatorhub.io on $CI_OHIO_BRANCH branch\"}}"  \
-     https://api.travis-ci.com/repo/operator-framework%2Foperatorhub.io/requests
+  --url https://api.github.com/repos/operator-framework/operatorhub.io/actions/workflows/4934257/dispatches \
+  --header "Authorization: token $REPO_GHA_PAT" \
+  --header 'Content-Type: application/json' \
+  --data "{
+    'ref': \"$CI_OHIO_BRANCH\"
+}"
 echo -e "\nCI on operator-framework/operatorhub.io and it's '$CI_OHIO_BRANCH' branch was triggered"
