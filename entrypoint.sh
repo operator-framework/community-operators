add07d2c7f07f5de43111b7e999a7ef157f87f85
j0zi <jbreza@redhat.com>
2021-04-23 10:58:52 +0200

    5
    Signed-off-by: j0zi <jbreza@redhat.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -65,7 +65,7 @@ else
 fi
 
 WEBHOOK_SIGNATURE=$(echo -n "$WEBHOOK_DATA" | openssl sha1 -hmac "$webhook_secret" -binary | xxd -p)
-WEBHOOK_SIGNATURE_256=$(echo -n "$WEBHOOK_DATA" | openssl dgst -sha256 -hmac "$webhook_secret" -binary | xxd -p |tr '\n' '')
+WEBHOOK_SIGNATURE_256=$(echo -n "$WEBHOOK_DATA" | openssl dgst -sha256 -hmac "$webhook_secret" -binary | xxd -p |tr -d '\n')
 echo $WEBHOOK_SIGNATURE_256
 WEBHOOK_ENDPOINT=$webhook_url
 
