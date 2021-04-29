ad53714ff40e12d6a29b97c67d7f2a327bc96d30
j0zi <jbreza@redhat.com>
2021-04-29 11:49:52 +0200

    failure report
    Signed-off-by: j0zi <jbreza@redhat.com>

--- scripts/ci/openshift-deploy.sh
+++ scripts/ci/openshift-deploy.sh
@@ -136,6 +136,11 @@ if [ $? -eq 0 ]; then
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/operator-framework/community-operators/dispatches --data "{\"event_type\": \"openshift-test-status\", \"client_payload\": {\"source_pr\": \"$PULL_NUMBER\", \"remove_labels\": [\"openshift-started\", \"deployment-ok\"], \"add_labels\": [\"deployment-ok\"]}}"
+else
+  curl -f -u framework-automation:$(cat /var/run/cred/framautom) \
+  -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/operator-framework/community-operators/dispatches --data "{\"event_type\": \"openshift-test-status\", \"client_payload\": {\"source_pr\": \"$PULL_NUMBER\", \"remove_labels\": [\"openshift-started\", \"deployment-ok\"], \"add_labels\": [\"openshift-failed\"]}}"
 fi
 
 echo "Variable summary:"
