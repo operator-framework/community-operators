f0be988283af2dfcb84568028f7dd53982fa16a1
Test User <test@example.com>
2021-05-21 14:38:17 +0200

    exit status openshift
    Signed-off-by: Test User <test@example.com>

--- scripts/ci/openshift-deploy.sh
+++ scripts/ci/openshift-deploy.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 #This scripts is configured in https://github.com/openshift/release/tree/master/ci-operator/config/operator-framework/community-operators and executed from ci-operator/jobs
 cd -P -- "$(dirname -- "$0")"
-./openshift-deploy-core.sh || curl -f -u framework-automation:$(cat /var/run/cred/framautom) \
+./openshift-deploy-core.sh || { curl -f -u framework-automation:$(cat /var/run/cred/framautom) \
 -X POST \
 -H "Accept: application/vnd.github.v3+json" \
-https://api.github.com/repos/operator-framework/community-operators/dispatches --data "{\"event_type\": \"openshift-test-status\", \"client_payload\": {\"source_pr\": \"$PULL_NUMBER\", \"remove_labels\": [\"openshift-started\", \"installation-validated\"], \"add_labels\": [\"installation-failed\"]}}"
-
-
+https://api.github.com/repos/operator-framework/community-operators/dispatches --data "{\"event_type\": \"openshift-test-status\", \"client_payload\": {\"source_pr\": \"$PULL_NUMBER\", \"remove_labels\": [\"openshift-started\", \"installation-validated\"], \"add_labels\": [\"installation-failed\"]}}";
+exit 1; }
