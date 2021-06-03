38313e0d0a790578752e130e14149db3664ca42a
J0zi <jbreza@redhat.com>
2021-06-03 14:20:46 +0200

    ocp test prerequisites for early exits (#3972)
    * ocp test prerequisites for early exits
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * test command
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * housekeeping
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * housekeeping
    
    Signed-off-by: J0zi <jbreza@redhat.com>
    
    * parametrized PR
    
    Signed-off-by: J0zi <jbreza@redhat.com>

--- scripts/ci/openshift-deploy-core.sh
+++ scripts/ci/openshift-deploy-core.sh
@@ -1,33 +1,39 @@
 #!/usr/bin/env bash
+#./openshift-deploy.sh test-only https://github.com/J0zi/community-operators.git bundle2 https://github.com/J0zi/operator-test-playbooks.git CVP-1793-exit-non-relevant-ocp-test
+
 set -e #fail in case of non zero return
 echo "OCP_CLUSTER_VERSION_SUFFIX=$OCP_CLUSTER_VERSION_SUFFIX"
 
 JQ_VERSION='1.6'
 MAX_LIMIT_FOR_INDEX_WAIT=20
 EXTRA_ARGS=''
+CURRENT_OPENSHIFT_RUN=${OCP_CLUSTER_VERSION-""}
 
 OC_DIR_CORE=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 20 | head -n 1)
 SUBDIR_ARG="-e work_subdir_name=oc-$OC_DIR_CORE"
 echo "SUBDIR_ARG = $SUBDIR_ARG"
 
 #label start
-curl -f -u framework-automation:$(cat /var/run/cred/framautom) \
+[[ $TEST_MODE -ne 1 ]] && curl -f -u framework-automation:$(cat /var/run/cred/framautom) \
 -X POST \
 -H "Accept: application/vnd.github.v3+json" \
 https://api.github.com/repos/operator-framework/community-operators/dispatches --data "{\"event_type\": \"openshift-test-status\", \"client_payload\": {\"source_pr\": \"$PULL_NUMBER\", \"remove_labels\": [\"installation-validated$OCP_CLUSTER_VERSION_SUFFIX\", \"openshift-started$OCP_CLUSTER_VERSION_SUFFIX\", \"installation-failed$OCP_CLUSTER_VERSION_SUFFIX\"], \"add_labels\": [\"openshift-started$OCP_CLUSTER_VERSION_SUFFIX\"]}}"
 
 pwd
-TARGET_PATH='/go/src/github.com/operator-framework/community-operators/community-operators'
+[[ $TEST_MODE -ne 1 ]] && TARGET_PATH='/go/src/github.com/operator-framework/community-operators/community-operators'
+[[ $TEST_MODE -eq 1 ]] && TARGET_PATH='/tmp/oper-for-me-test/community-operators/community-operators'
 
-##temp test for development to test on a stable commit
-#echo "Need to clone test branch, cloning..."
-#mkdir -p /tmp/oper-for-me-test
-#cd /tmp/oper-for-me-test
-#git clone https://github.com/J0zi/community-operators.git
-#cd community-operators
-#git checkout oper-for-my-test
-#ls
-#TARGET_PATH='/tmp/oper-for-me-test/community-operators/community-operators'
+#temp test for development to test on a stable commit
+if [ $TEST_MODE -eq 1 ]; then
+  echo "Need to clone test branch, cloning..."
+  if [ -d /tmp/oper-for-me-test ]; then rm -Rf /tmp/oper-for-me-test; fi
+  mkdir -p /tmp/oper-for-me-test
+  cd /tmp/oper-for-me-test
+  git clone $TEST_COMMUNITY_REPO
+  cd community-operators
+  git checkout $TEST_COMMUNITY_BRANCH
+  ls
+fi
 
 ##clone again to suppress caching during pr on ci-operator repo
 #echo "Need to clone actual branch, cloning..."
@@ -44,22 +50,20 @@ curl -L "https://github.com/stedolan/jq/releases/download/jq-$JQ_VERSION/jq-linu
 chmod +x "/tmp/jq-$OC_DIR_CORE/bin/jq" && echo "rights adjusted"
 
 #detect allow/longer-deployment label
-curl -f -u framework-automation:$(cat /var/run/cred/framautom) \
+[[ $TEST_MODE -ne 1 ]] && curl -f -u framework-automation:$(cat /var/run/cred/framautom) \
 -X GET \
 -H "Accept: application/vnd.github.v3+json" \
 "https://api.github.com/repos/operator-framework/community-operators/issues/$PULL_NUMBER"|"/tmp/jq-$OC_DIR_CORE/bin/jq" '.labels[].name'|grep 'allow/longer-deployment' \
 && echo "Longer deployment detected" && EXTRA_ARGS='-e pod_start_retries=300'
-
 cd "$TARGET_PATH"
-
 tmpfile=$(mktemp /tmp/pr-details-XXXXXXX.json)
 curl -s https://api.github.com/repos/operator-framework/community-operators/pulls/$PULL_NUMBER -o $tmpfile
+cat $tmpfile
 REPO_FULL=$(cat $tmpfile | /tmp/jq-$OC_DIR_CORE/bin/jq -r '.head.repo.clone_url')
 BRANCH=$(cat $tmpfile | /tmp/jq-$OC_DIR_CORE/bin/jq -r '.head.ref')
 COMMIT=$(cat $tmpfile | /tmp/jq-$OC_DIR_CORE/bin/jq -r '.head.sha')
 REPO=$(echo "$REPO_FULL"| awk -F'https://github.com/' '{print $2}')
 QUAY_HASH=$(echo ${COMMIT::8})
-
 rm -f $tmpfile > /dev/null 2>&1
 
 OPRT_REPO=${REPO_FULL-""}
@@ -69,11 +73,10 @@ export OPRT=1
 
 [ -n "$OPRT_REPO" ] || { echo "Error: '\$OPRT_REPO' is empty !!!"; exit 1; }
 [ -n "$OPRT_SHA" ] || { echo "Error: '\$OPRT_SHA' is empty !!!"; exit 1; }
-
 git clone $REPO_FULL community-operators > /dev/null 2>&1
 cd community-operators
 BRANCH_NAME=$(git branch -a --contains $OPRT_SHA | grep remotes/ | grep -v HEAD | cut -d '/' -f 2-)
-git checkout $BRANCH_NAME > /dev/null 2>&1
+git checkout $BRANCH_NAME > /dev/null #2>&1
 git log --oneline | head
 
 git config --global user.email "test@example.com"
@@ -129,9 +132,9 @@ echo "OP_VER=$OP_VER"
 #echo "Forced specific operator - $OP_NAME $OP_VER $COMMIT"
 
 #prepare temp index
-OP_TOKEN=$(cat /var/run/cred/op_token_quay_test)
+[[ $TEST_MODE -ne 1 ]] && OP_TOKEN=$(cat /var/run/cred/op_token_quay_test)
 echo
-curl -f -u framework-automation:$(cat /var/run/cred/framautom) \
+[[ $TEST_MODE -ne 1 ]] && curl -f -u framework-automation:$(cat /var/run/cred/framautom) \
 -X POST \
 -H "Accept: application/vnd.github.v3+json" \
 https://api.github.com/repos/operator-framework/community-operators/dispatches --data "{\"event_type\": \"index-for-openshift-test\", \"client_payload\": {\"source_pr\": \"$PULL_NUMBER\"}}"  && echo "Temp index initiated ..."
@@ -155,16 +158,21 @@ while [ "$CHECK_TEMP_INDEX" -le "$MAX_LIMIT_FOR_INDEX_WAIT" ]; do
 done
 
 #deploy start
+if [ -d /tmp/playbooks2 ]; then rm -Rf /tmp/playbooks2; fi
 mkdir -p /tmp/playbooks2
 cd /tmp/playbooks2
-git clone https://github.com/operator-framework/operator-test-playbooks.git
-cd operator-test-playbooks/upstream
+[[ $TEST_MODE -ne 1 ]] && git clone https://github.com/operator-framework/operator-test-playbooks.git
+[[ $TEST_MODE -eq 1 ]] && git clone $TEST_PB_REPO
+cd operator-test-playbooks
+[[ $TEST_MODE -eq 1 ]] && git checkout $TEST_PB_BRANCH
+cd upstream
+
 export ANSIBLE_CONFIG=/tmp/playbooks2/operator-test-playbooks/upstream/ansible.cfg
 set +e
-ANSIBLE_STDOUT_CALLBACK=yaml ansible-playbook -i localhost, deploy-olm-operator-openshift-upstream.yml -e ansible_connection=local -e package_name=$OP_NAME -e operator_dir=$TARGET_PATH/$OP_NAME -e op_version=$OP_VER -e oc_bin_path="/tmp/oc-$OC_DIR_CORE/bin/oc" -e commit_tag=$QUAY_HASH -e dir_suffix_part=$OC_DIR_CORE $SUBDIR_ARG $EXTRA_ARGS -vv
+ANSIBLE_STDOUT_CALLBACK=yaml ansible-playbook -i localhost, deploy-olm-operator-openshift-upstream.yml -e ansible_connection=local -e package_name=$OP_NAME -e operator_dir=$TARGET_PATH/$OP_NAME -e op_version=$OP_VER -e oc_bin_path="/tmp/oc-$OC_DIR_CORE/bin/oc" -e commit_tag=$QUAY_HASH -e dir_suffix_part=$OC_DIR_CORE -e current_openshift_run=$CURRENT_OPENSHIFT_RUN $SUBDIR_ARG $EXTRA_ARGS -vv
 ANSIBLE_STATUS=$?
 
-if [ $ANSIBLE_STATUS -eq 0 ]; then
+[[ $TEST_MODE -ne 1 ]] && if [ $ANSIBLE_STATUS -eq 0 ]; then
   curl -f -u framework-automation:$(cat /var/run/cred/framautom) \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
