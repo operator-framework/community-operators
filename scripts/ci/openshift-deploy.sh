a762c05860099345bf53ec819e5a070455ce5d3c
J0zi <jbreza@redhat.com>
2021-06-09 15:23:01 +0200

    fix final label (#4033)
    Signed-off-by: J0zi <jbreza@redhat.com>

--- scripts/ci/openshift-deploy.sh
+++ scripts/ci/openshift-deploy.sh
@@ -25,8 +25,8 @@ else export OCP_CLUSTER_VERSION_SUFFIX=""
 fi
 
 cd -P -- "$(dirname -- "$0")"
-./openshift-deploy-core.sh || { [[ $TEST_MODE -ne 1 ]] && { curl -f -u framework-automation:$(cat /var/run/cred/framautom) \
+./openshift-deploy-core.sh || curl -f -u framework-automation:$(cat /var/run/cred/framautom) \
 -X POST \
 -H "Accept: application/vnd.github.v3+json" \
 https://api.github.com/repos/operator-framework/community-operators/dispatches --data "{\"event_type\": \"openshift-test-status\", \"client_payload\": {\"source_pr\": \"$PULL_NUMBER\", \"remove_labels\": [\"openshift-started$OCP_CLUSTER_VERSION_SUFFIX\", \"installation-validated$OCP_CLUSTER_VERSION_SUFFIX\"], \"add_labels\": [\"installation-failed$OCP_CLUSTER_VERSION_SUFFIX\"]}}";
-exit 1; } }
+exit 1;
