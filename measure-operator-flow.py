d5a5e3fa06ee83ec8b46f42b51d7d3d44280b620
Khaled Sulayman <ksulayma@redhat.com>
2021-06-11 15:16:51 -0400

    add graphql query to script

--- measure-operator-flow.py
+++ measure-operator-flow.py
@@ -1,5 +1,79 @@
 import requests
 from datetime import datetime
+from string import Template
+
+
+def build_comment_query(cursor):
+    return Template("""
+        comments(first: 100, after: $cursor) {
+            pageInfo {
+                hasNextPage
+                endCursor
+            }
+            nodes {
+                author { login }
+            }
+        }
+    """).substitute({'cursor': cursor})
+
+
+def build_timeline_query(cursor):
+    return Template("""
+        timelineItems(itemTypes: LABELED_EVENT, first: 100, after: $cursor) {
+            pageInfo {
+                hasNextPage
+                endCursor
+            }
+            nodes {
+                ... on LabeledEvent {
+                    actor { login }
+                    label { name }
+                }
+            }
+        }
+    """).substitute({'cursor': cursor})
+
+
+def build_query(pr_cursor=0, comment_cursor=0, timeline_cursor=0):
+    """Build query for a single call to GitHub API
+
+    Parameters:
+        pr_cursor (int): Cursor of last requested Pull-Request
+        comment_cursor (int): Cursor of last requested Comment
+        timeline_cursor (int): Cursor of last requested Timeline
+    """
+    comment_query = build_comment_query(comment_cursor)
+    timeline_query = build_timeline_query(timeline_cursor)
+
+    query = Template("""query {
+        repository(owner: “operator-framework”, name: “community-operators”) {
+            pullRequests(states: MERGED, first: 100, after: $cursor) {
+                pageInfo {
+                    hasNextPage
+                    endCursor
+                }
+                nodes {
+                    number
+                    createdAt
+                    mergedAt
+                    author { login }
+                    $comment_query
+                    $timeline_query
+                }
+            }
+        }
+        rateLimit {
+            limit
+            cost
+            remaining
+            resetAt
+        }
+    }
+    """).substitute({'cursor': pr_cursor,
+                     'comment_query': comment_query,
+                     'timeline_query': timeline_query})
+    
+    return query
 
 
 def _get_pull_requests(owner, repo, page, per_page=100):
@@ -41,4 +115,4 @@ def get_pull_requests(owner='operator-framework', repo='community-operators',
 
 
 if __name__ == '__main__':
-    print(get_pull_requests())
+    print(build_query())
