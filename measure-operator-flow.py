7b4bf724d3a562b1f314ca12e0b0fa835fca9f67
Khaled Sulayman <ksulayma@redhat.com>
2021-06-16 16:21:38 -0400

    process PR data into desired format

--- measure-operator-flow.py
+++ measure-operator-flow.py
@@ -3,6 +3,14 @@ import pprint
 from graphql_handler import get_pr_data
 
 
+# list of 
+maintainers = set([
+    'maintainer'
+    'J0zi',
+    'mvalarh',
+])
+
+
 def compute_duration(created_at, merged_at):
     """Compute duration that a merged PR was open (in seconds)"""
     format_str = r"%Y-%m-%dT%H:%M:%SZ"
@@ -12,13 +20,42 @@ def compute_duration(created_at, merged_at):
     return int(time_diff)
 
 
-def main():
-    pr_data = get_pr_data()
-    pp = pprint.PrettyPrinter()
-    pp.pprint(pr_data)
-    print(f'{len(pr_data)} PRs queried')
+def process_pr_data(pr_data):
+    processed_pr_data = []
 
-    return pr_data
+    for number, pr in pr_data.items():
+        time_to_merge = compute_duration(pr['created_at'], pr['merged_at'])
+
+        if pr['labeler'] in maintainers:
+            authorizer = 'maintainer'
+        else:
+            authorizer = 'bot'
+
+        if maintainers & pr['commentors']:
+            interaction = 'comment by maintainer'
+        elif authorizer == 'maintainer':
+            interaction = 'maintainer set `authorized-changes`'
+        else:
+            interaction = 'none'
+
+        processed_pr_data.append({
+            'number': number,
+            'date created': pr['created_at'],
+            'time to merge': time_to_merge,
+            'authorizer': authorizer,
+            'interaction': interaction
+        })
+    
+    return processed_pr_data
+
+
+def main():
+    pr_data = get_pr_data(rate_limit=True)
+    processed_pr_data = process_pr_data(pr_data)
+
+    pp = pprint.PrettyPrinter()
+    pp.pprint(processed_pr_data)
+    print(f'{len(pr_data)} PRs queried')
 
 
 if __name__ == '__main__':
