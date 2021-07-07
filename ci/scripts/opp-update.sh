c528fc096cafb3815857ac7dc15d34c3c53a0f4d
Martin Vala <mavala@redhat.com>
2021-07-07 16:55:42 +0200

    Test
    Signed-off-by: Martin Vala <mavala@redhat.com>

--- ci/scripts/opp-update.sh
+++ ci/scripts/opp-update.sh
@@ -11,6 +11,6 @@ OPP_ANSIBLE_ARGS="-vv upstream/set-pipeline-workflow.yml -e ansible_connection=l
 OPP_TMP_DIR="/tmp/opp-update"
 [ -d $OPP_TMP_DIR ] && rm -rf $OPP_TMP_DIR
 mkdir -p $OPP_TMP_DIR
-git clone $OPP_INPUT_REPO --branch $OPP_INPUT_BRANCH $OPP_TMP_DIR/opp-input
+git clone https://github.com/$OPP_INPUT_REPO --branch $OPP_INPUT_BRANCH $OPP_TMP_DIR/opp-input
 
 ansible-pull -U $OPP_ANSIBLE_PULL_REPO -C $OPP_ANSIBLE_PULL_BRANCH $OPP_ANSIBLE_ARGS  -e workflow_templates_path=$OPP_TMP_DIR/opp-input/scripts/template/workflow/ -e workflow_config_path=$PWD
