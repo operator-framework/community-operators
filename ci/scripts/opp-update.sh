9e44f87e4993b91588b8fe257f69ccdd646fb90d
Martin Vala <mavala@redhat.com>
2021-07-07 17:51:58 +0200

    Improved workflows
    Signed-off-by: Martin Vala <mavala@redhat.com>

--- ci/scripts/opp-update.sh
+++ ci/scripts/opp-update.sh
@@ -7,10 +7,13 @@ OPP_ANSIBLE_PULL_BRANCH=${OPP_ANSIBLE_PULL_BRANCH-"upstream-community"}
 OPP_INPUT_REPO=${OPP_INPUT_REPO-"https://github.com/operator-framework/community-operators"}
 OPP_INPUT_BRANCH=${OPP_INPUT_BRANCH-"master"}
 
-OPP_ANSIBLE_ARGS="-i localhost, -e ansible_connection=local upstream/set-pipeline-workflow.yml"
+OPP_ANSIBLE_ARGS="-i localhost, -e ansible_connection=local upstream/local-pipeline-update.yml"
 OPP_TMP_DIR="/tmp/opp-update"
 [ -d $OPP_TMP_DIR ] && rm -rf $OPP_TMP_DIR
 mkdir -p $OPP_TMP_DIR
 git clone $OPP_INPUT_REPO --branch $OPP_INPUT_BRANCH $OPP_TMP_DIR/opp-input
 
-ANSIBLE_STDOUT_CALLBACK=yaml ansible-pull -U $OPP_ANSIBLE_PULL_REPO -C $OPP_ANSIBLE_PULL_BRANCH $OPP_ANSIBLE_ARGS  -e workflow_templates_path=$OPP_TMP_DIR/opp-input/scripts/template/workflow/ -e workflow_config_path=$PWD/ci
+ANSIBLE_STDOUT_CALLBACK=yaml ansible-pull -U $OPP_ANSIBLE_PULL_REPO -C $OPP_ANSIBLE_PULL_BRANCH $OPP_ANSIBLE_ARGS \
+-e workflow_config_path="$PWD/ci" \
+-e workflow_templates_path="$OPP_TMP_DIR/opp-input/scripts/template/workflow" \
+-e workflow_output_path="$PWD/.github/workflows"
