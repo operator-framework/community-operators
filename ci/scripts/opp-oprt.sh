9973e0c4952fb3730ab97920b7911c784979b317
J0zi <jbreza@redhat.com>
2021-07-12 17:17:58 +0200

    verbosity
    Signed-off-by: J0zi <jbreza@redhat.com>

--- ci/scripts/opp-oprt.sh
+++ ci/scripts/opp-oprt.sh
@@ -13,7 +13,7 @@ echo "OPP_SCRIPT_ENV_URL=$OPP_SCRIPT_ENV_URL"
 [ -n "$OPP_OPRT_REPO" ] || { echo "Error: '\$OPP_OPRT_REPO' is empty !!!"; exit 1; }
 [ -n "$OPP_OPRT_SHA" ] || { echo "Error: '\$OPP_OPRT_SHA' is empty !!!"; exit 1; }
 
-git clone https://github.com/$OPP_OPRT_REPO community-operators > /dev/null 2>&1
+git clone https://github.com/$OPP_OPRT_REPO community-operators #> /dev/null 2>&1
 echo "cloned https://github.com/$OPP_OPRT_REPO"
 cd community-operators
 BRANCH_NAME=$(git branch -a --contains $OPP_OPRT_SHA | grep remotes/ | grep -v HEAD | cut -d '/' -f 2-)
@@ -24,7 +24,7 @@ git log --oneline | head
 git config --global user.email "test@example.com"
 git config --global user.name "Test User"
 
-git remote add upstream https://github.com/$OPP_OPRT_SRC_REPO -f > /dev/null 2>&1
+git remote add upstream https://github.com/$OPP_OPRT_SRC_REPO -f #> /dev/null 2>&1
 echo "added remote https://github.com/$OPP_OPRT_SRC_REPO"
 git pull --rebase -Xours upstream $OPP_OPRT_SRC_BRANCH 
 echo "Repo rebased over branch OPP_OPRT_SRC_BRANCH - $OPP_OPRT_SRC_BRANCH"
