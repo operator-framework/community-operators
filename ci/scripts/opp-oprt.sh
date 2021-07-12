12d90bf4b55ba33f8df269bca63f29186d65e3f2
J0zi <jbreza@redhat.com>
2021-07-12 16:10:17 +0200

    verbose
    Signed-off-by: J0zi <jbreza@redhat.com>

--- ci/scripts/opp-oprt.sh
+++ ci/scripts/opp-oprt.sh
@@ -14,8 +14,10 @@ echo "OPP_SCRIPT_ENV_URL=$OPP_SCRIPT_ENV_URL"
 [ -n "$OPP_OPRT_SHA" ] || { echo "Error: '\$OPP_OPRT_SHA' is empty !!!"; exit 1; }
 
 git clone https://github.com/$OPP_OPRT_REPO community-operators > /dev/null 2>&1
+echo "cloned https://github.com/$OPP_OPRT_REPO"
 cd community-operators
 BRANCH_NAME=$(git branch -a --contains $OPP_OPRT_SHA | grep remotes/ | grep -v HEAD | cut -d '/' -f 2-)
+echo "BRANCH_NAME=$BRANCH_NAME"
 git checkout $BRANCH_NAME > /dev/null 2>&1
 git log --oneline | head
 
@@ -23,6 +25,7 @@ git config --global user.email "test@example.com"
 git config --global user.name "Test User"
 
 git remote add upstream https://github.com/$OPP_OPRT_SRC_REPO -f > /dev/null 2>&1
+echo "added remote https://github.com/$OPP_OPRT_SRC_REPO"
 git pull --rebase -Xours upstream $OPP_OPRT_SRC_BRANCH 
 
 export OPP_ADDED_FILES=$(git diff --diff-filter=A upstream/$OPP_OPRT_SRC_BRANCH --name-only | tr '\r\n' ' ')
