1d7661b1be9745a501ba26ff15994d9ac7dbda4a
J0zi <jbreza@redhat.com>
2021-07-12 17:38:20 +0200

    HEAD
    Signed-off-by: J0zi <jbreza@redhat.com>

--- ci/scripts/opp-oprt.sh
+++ ci/scripts/opp-oprt.sh
@@ -30,11 +30,11 @@ git pull --rebase -Xours upstream $OPP_OPRT_SRC_BRANCH
 echo "Repo rebased over branch OPP_OPRT_SRC_BRANCH - $OPP_OPRT_SRC_BRANCH"
 
 export OPP_ADDED_FILES=$(git diff --diff-filter=A upstream/$OPP_OPRT_SRC_BRANCH --name-only | tr '\r\n' ' ')
-export OPP_MODIFIED_FILES=$(git diff --diff-filter=M upstream/$OPP_OPRT_SRC_BRANCH --name-only | tr '\r\n' ' ')
+export OPP_MODIFIED_FILES=$(git diff --diff-filter=M HEAD upstream/$OPP_OPRT_SRC_BRANCH --name-only | tr '\r\n' ' ')
 export OPP_REMOVED_FILES=$(git diff --diff-filter=D upstream/$OPP_OPRT_SRC_BRANCH --name-only | tr '\r\n' ' ')
 export OPP_RENAMED_FILES=$(git diff --diff-filter=R upstream/$OPP_OPRT_SRC_BRANCH --name-only | tr '\r\n' ' ')
 export OPP_ADDED_MODIFIED_FILES=$(git diff --diff-filter=AM upstream/$OPP_OPRT_SRC_BRANCH --name-only | tr '\r\n' ' ')
-export OPP_ADDED_MODIFIED_RENAMED_FILES=$(git diff --diff-filter=RAM HEAD upstream/$OPP_OPRT_SRC_BRANCH --name-only | tr '\r\n' ' ')
+export OPP_ADDED_MODIFIED_RENAMED_FILES=$(git diff --diff-filter=RAM upstream/$OPP_OPRT_SRC_BRANCH --name-only | tr '\r\n' ' ')
 export OPP_CURRENT_PROJECT_REPO="$OPP_OPRT_SRC_REPO"
 export OPP_CURRENT_PROJECT_BRANCH="$OPP_OPRT_SRC_BRANCH"
 
